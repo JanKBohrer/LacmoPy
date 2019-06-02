long Kernel -> wrong results -> need parametr. of Efficiency E(r1,r2)

collision.py
@@ -13,8 +13,9 @@ import numpy as np
 from numba import njit
 import math
 
-
-
+import constants as c
+from microphysics import compute_radius_from_mass_jit
+from microphysics import compute_mass_from_radius
 
 
 # returns a permutation of the list of integers [0,1,2,..,N-1] by Shima method
@@ -39,10 +40,75 @@ def generate_permutation(N):
 def kernel_golovin(m_i, m_j):
     return 1.5E-18 * (m_i + m_j)
 
+# Long 1974 
+# Kernel (two versions (?) he says: might be approx by ... OR ... (?))
+# First:
+# R < 50 mu: 9.44E9 * (Vi^2 + Vj^2)
+# R > 50 mu: 5.78E3 * (Vi + Vj)
+# Second:
+# 1.1E10 * Vi^2 (no Vj)
+# 6.33E3 Vi     (no Vj)
+# here R = radius of LARGER drop, Vi = volume of larger drop in cm^3
+# implement first version:
+# R_i in mu
+# m_i in fg
+four_pi_over_three = 4.0/3.0*math.pi
+@njit()
+def kernel_long(m_i, m_j):
+    R_i = compute_radius_from_mass_jit(m_i, c.mass_density_water_liquid_NTP)
+    R_j = compute_radius_from_mass_jit(m_j, c.mass_density_water_liquid_NTP)
+    # R_max = max(R_i, R_j):
+    if R_i <= 50 and R_j <= 50:
+        # return 9.44E-15 * (m_i*m_i + m_j*m_j)\
+        #        / (c.mass_density_water_liquid_NTP
+        #           * c.mass_density_water_liquid_NTP)
+        # return 9.44E-19 * (m_i*m_i + m_j*m_j)\
+        #        / (c.mass_density_water_liquid_NTP
+        #           * c.mass_density_water_liquid_NTP)
+        # return 9.44E9*1.0E-24*(m_i*m_i + m_j*m_j)\
+        #        / (c.mass_density_water_liquid_NTP
+        #           * c.mass_density_water_liquid_NTP)
+        return 9.44E9 * 1.0E-24 * four_pi_over_three**2 * (R_i**6 + R_j**6)
+    else:
+        # return 5.78E-9 * (m_i + m_j) / c.mass_density_water_liquid_NTP
+        # return 5.78E-12 * (m_i + m_j) / c.mass_density_water_liquid_NTP
+        # return 5.78E3 * 1.0E-12 (m_i + m_j) / c.mass_density_water_liquid_NTP
+        return 5.78E3 * 1.0E-12 * four_pi_over_three * (R_i**3 + R_j**3)
+           
+# m_i = 1E6
+# # m_j = 1E7
+# R_i = compute_radius_from_mass_jit(m_i, c.mass_density_water_liquid_NTP)
+# # R_j = compute_radius_from_mass_jit(m_j, c.mass_density_water_liquid_NTP)
+# print(R_i)
+# # print(R_j)
+
+# kernel_lon = []
+# kernel_gol = []
+
+# # print(kernel_long(m_i,m_j))
+# R_range = np.arange(10,100,1)
+# m_range = compute_mass_from_radius(R_range, c.mass_density_water_liquid_NTP)
+# for m_j in m_range:
+#     kernel_gol.append(kernel_golovin(m_i, m_j))
+#     kernel_lon.append(kernel_long(m_i, m_j))
+
+# kernel_gol = np.array(kernel_gol)
+# kernel_lon = np.array(kernel_lon)
+
+# import matplotlib.pyplot as plt
+
+# # R_j = np.arange(10,100,10)
+# # R_range
+# plt.plot(m_range, kernel_gol,"o")
+# plt.plot(m_range, kernel_lon,"x")
+# m_50 = compute_mass_from_radius(50, c.mass_density_water_liquid_NTP)
+# plt.vlines(m_50,
+#            0.0, kernel_long(m_i,m_50))
+
 #%% SHIMA alg 01 ALPHA
     
 @njit()
-def collision_step(xi, masses, dV, dt):
+def collision_step_golovin(xi, masses, dV, dt):
     # xi is a 1D array of multiplicities
     # elements of xi can be 0, indicating vanished droplets
     # look for the indices, which are not zero
@@ -125,6 +191,90 @@ def collision_step(xi, masses, dV, dt):
                 masses[ind_min] += g * masses[ind_max]
                 masses[ind_max] = masses[ind_min]
 
+@njit()
+def collision_step_long(xi, masses, dV, dt):
+    # xi is a 1D array of multiplicities
+    # elements of xi can be 0, indicating vanished droplets
+    # look for the indices, which are not zero
+    indices = np.nonzero(xi)[0]
+    # N = no_spct ("number of super particles per cell total")
+    # no_spct = len(indices)
+    no_spct = indices.shape[0]
+    # no_spct = 5940
+    # Let I = [0,1,2,3,...,N-1] be the list of all EXISTING particle indices
+    
+    ### 1. make a random permutation of I by
+    permutation = generate_permutation(no_spct)
+    # print("permutation len")
+    # print(len(permutation))
+    
+    ### 2. make [no_spct/2] candidate pairs [(i1,j1),(i2,j2),(),.]
+    no_pairs = no_spct//2
+    cand_i = permutation[0:no_pairs]
+    cand_j = permutation[no_pairs:2*no_pairs]
+    # map onto the original xi-list, to get the indices for that list
+    
+    cand_i = indices[cand_i]
+    cand_j = indices[cand_j]
+    
+    # print("cand_i len")
+    # print(len(cand_i))
+    # print("cand_j len")
+    # print(len(cand_j))
+    
+    ### 3. for each pair (i,j) generate uniform float random number from [0,1)
+    rnd01 = np.random.rand(no_pairs)
+    # print("len(rnd01)")
+    # print(len(rnd01))
+    for pair_n in range(no_pairs):
+        i = cand_i[pair_n]
+        j = cand_j[pair_n]
+        # i_done.append(i)
+        # j_done.append(j)
+        pair_kernel =\
+            kernel_long(masses[i], masses[j])
+        if xi[i] >= xi[j]:
+            xi_max = xi[i]
+            ind_max = i
+            xi_min = xi[j]
+            ind_min = j
+        else:
+            xi_max = xi[j]
+            ind_max = j
+            xi_min = xi[i]
+            ind_min = i
+            
+        # xi_max = max(xi[i], xi[j])
+        # now we need p = P_ij,s N_s*(N_s-1)/2 / (no_pairs),
+        # where N_s = no_spct and
+        # P_ij,s = max(xi_i, xi_j) P_ij
+        # P_ij = K_ij dt/dV
+        pair_prob = pair_kernel * dt/dV * xi_max\
+                    * 0.5 * no_spct * (no_spct - 1) / no_pairs
+        # then evaluate
+        # beta
+        # = [p] + 1; if rnd01 < p - [p]
+        # = [p]    ; if rnd01 >= p - [p]
+        pair_prob_int = int(pair_prob)
+        if rnd01[pair_n] < pair_prob - pair_prob_int:
+            g = pair_prob_int + 1
+        else: g = pair_prob_int
+        # print(pair_n, pair_prob, pair_prob_int, rnd01[pair_n], g)
+        if g > 0:
+            # print("collision for pair", i, j)
+            # xi_min = min(xi[i], xi[j])
+            g = min(g,int(xi_max/xi_min))
+            if xi_max - g*xi_min > 0:
+                # print("case 1")
+                xi[ind_max] -= g * xi_min
+                masses[ind_min] += g * masses[ind_max]
+            else:
+                # print("case 2")
+                xi[ind_max] = int(0.5 * xi_min)
+                xi[ind_min] -= int(0.5 * xi_min)
+                masses[ind_min] += g * masses[ind_max]
+                masses[ind_max] = masses[ind_min]
+
 #%%
 
 # dV = 1.0E-6
@@ -163,7 +313,10 @@ def collision_step(xi, masses, dV, dt):
 
 #%%
 
-def simulate_collisions_np(xi, masses, dV, dt, t_end, store_every):
+def simulate_collisions_np(xi, masses, dV, dt, t_end, store_every,
+                           kernel="golovin"):
+    if kernel == "long": collision_step = collision_step_long
+    elif kernel == "golovin": collision_step = collision_step_golovin
     times = np.zeros(1+int(t_end/(store_every * dt)), dtype = np.float64)
     concentrations = np.zeros(1+int( math.ceil(t_end / (store_every * dt)) ),
                               dtype = np.float64)
@@ -189,7 +342,7 @@ def simulate_collisions_np(xi, masses, dV, dt, t_end, store_every):
     xis_vs_time[cnt_c] = xi
     
     return times, concentrations, masses_vs_time, xis_vs_time
-simulate_collisions = njit()(simulate_collisions_np)
+# simulate_collisions = njit()(simulate_collisions_np)
 
 
 

collision_box_model2.py
@@ -53,19 +53,16 @@ import os
 import constants as c
 # from microphysics import compute_mass_from_radius
 from microphysics import compute_radius_from_mass
-# from collision import simulate_collisions_np
-from collision import simulate_collisions
+from collision import simulate_collisions_np
+# from collision import simulate_collisions
 from init import dst_expo
-from init import generate_SIP_ensemble
+from init import generate_SIP_ensemble_expo_my_xi_rnd
+from init import generate_SIP_ensemble_expo_SingleSIP_weak_threshold
 # from collision import generate_permutation
 # from collision import simulate_collisions
 
 #%% INIT
-simdata_path = "/home/jdesk/OneDrive/python/sim_data/"
-folder = "collision_box_model_multi_sim/dV_1E0_no_spc_80_no_sim_50_eps_200_pmax_1E-7/"
-path = simdata_path + folder
-if not os.path.exists(path):
-    os.makedirs(path)
+
 
 #dx = 1 # m
 #dy = 1 # m
@@ -93,7 +90,35 @@ dt = 1.0
 store_every = 600
 t_end = 3600.0
 
-no_spc = 80
+no_spc = 40
+
+kernel = "long"
+init = "SingleSIP"
+
+no_sims = 50
+seed_list = np.arange(4711, 4711+no_sims*2, 2)
+# seed_list = np.arange(4711+38, 4711+38+no_sims*2, 2)
+
+p_min = 0
+p_max = 0.9999999
+# bin linear spreading parameter
+eps = 200
+
+# for SingleSIP random
+kappa = 40
+
+simdata_path = "/home/jdesk/OneDrive/python/sim_data/"
+# folder = "collision_box_model_multi_sim/dV_1E0_no_spc_80_no_sim_50_eps_200_pmax_1E-7/"
+# folder = "collision_box_model_multi_sim/dV_1E0_kappa_40_no_sim_50/"
+if init == "SingleSIP":
+    folder =\
+f"collision_box_model/kernels/{kernel}/init_{init}_dV_{dV}_kappa_{kappa}_no_sims_{no_sims}/"
+elif init == "xi_random":
+    folder =\
+f"collision_box_model/kernels/{kernel}/init_{init}_dV_{dV}_no_spc_{no_spc}_eps_{eps}no_sims_{no_sims}/"
+path = simdata_path + folder
+if not os.path.exists(path):
+    os.makedirs(path)
 
 # droplet concentration
 #n = 100 # cm^(-3)
@@ -122,14 +147,7 @@ total_mass_in_cell = dV*LWC0*1.0E6*1.0E15 # in fg = 1.0E-18 kg
 # mu = compute_mass_from_radius(mu_R, c.mass_density_water_liquid_NTP)
 # print(mu)
 
-no_sims = 50
-seed_list = np.arange(4711, 4711+no_sims*2, 2)
-# seed_list = np.arange(4711+38, 4711+38+no_sims*2, 2)
 
-p_min = 0
-p_max = 0.9999999
-# bin linear spreading parameter
-eps = 200
 dm = mu*1.0E-5
 m0 = 0.0
 m1 = 100*mu
@@ -140,20 +158,26 @@ for sim_n in range(no_sims):
     dst = dst_expo
     par = 1/mu
     
-    masses, xi, m_low, m_high =\
-        generate_SIP_ensemble(dst, par, no_spc, no_rpc,
-                              total_mass_in_cell,
-                              p_min, p_max, eps,
-                              m0, m1, dm, seed, setseed = True)
+    masses, xis, m_low, m_high =\
+        generate_SIP_ensemble_expo_SingleSIP_weak_threshold(
+                              1.0/mu, no_rpc, kappa=kappa, seed = seed)    
+        # generate_SIP_ensemble_expo_my_xi_rnd(dst, par, no_spc, no_rpc,
+        #                       total_mass_in_cell,
+        #                       p_min, p_max, eps,
+        #                       m0, m1, dm, seed, setseed = True)
     
     # masses = np.random.exponential(mu, no_rpc) # in mu
     # xi = np.ones(no_rpc, dtype = np.int64)
     
+    
     m_max = np.amax(masses)
     print("sim_n", sim_n, "; masses.shape=", masses.shape, "; m_max=", m_max,
-          "m_true-m_sys=", total_mass_in_cell-np.sum(masses*xi),
-          "no_pt_true-no_sys=", no_rpc - np.sum(xi) )
+          "m_true-m_sys (%)=",
+          (total_mass_in_cell-np.sum(masses*xis))/total_mass_in_cell*100,
+          "no_pt_true-no_sys (%)=", (no_rpc - np.sum(xis))/no_rpc*100 )
     print("m_high=", f"{m_high:.2e}", "m_low=", m_low)
+    
+    radii = compute_radius_from_mass(masses, c.mass_density_water_liquid_NTP)
     
     ###
 #     fig_name = f"mass_distribution_init_{sim_n}.png"
@@ -179,22 +203,28 @@ for sim_n in range(no_sims):
     ###
     
     ###
-    fig_name = f"SIP_ensemble_dV_{dV}_no_spc_aim_{no_spc}_sim_no_{sim_n}.png"
+    # fig_name = f"SIP_ensemble_dV_{dV}_no_spc_aim_{no_spc}_sim_no_{sim_n}.png"
+    fig_name = f"SIP_ensemble_dV_{dV}_kappa_{kappa}_sim_no_{sim_n}.png"
     fig, ax = plt.subplots(figsize=(8,8))
-    ax.plot(masses, xi, "o")
-    m_max = np.amax(masses)
-    m_ = np.linspace(0.0,m_max, 10000)
+    # ax.plot(masses, xi, "o")
+    ax.plot(radii, xis, "x-")
+    # m_max = np.amax(masses)
+    # m_ = np.linspace(0.0,m_max, 10000)
     # m_max = masses[-2]
-    m_min = np.amin(masses)
+    # m_min = np.amin(masses)
     # no_spc = len(masses)
-    no_rpc = xi.sum()
+    # no_rpc = xi.sum()
     # m_ges = np.sum(masses*xis)
     # bin_size = m_max/no_spc
     # bin_size = m_max/(no_spc-1)
-    bin_size = (m_high - m_low)/(no_spc)
+    
+    # bin_size = (m_high - m_low)/(no_spc)
+    
     # ax.plot(m_, no_rpc*np.exp(-m_/mu)\
     #             *(np.exp(0.5*bin_size/mu)-np.exp(-0.5*bin_size/mu)))
-    ax.plot(m_, no_rpc*bin_size*dst_expo(m_,1.0/mu))
+    
+    # ax.plot(m_, no_rpc*bin_size*dst_expo(m_,1.0/mu))
+    
     ax.set_xscale("log")
     ax.set_yscale("log")
     ###
@@ -205,8 +235,9 @@ for sim_n in range(no_sims):
     # print("start sim here")
 
     times, concentrations, masses_vs_time, xis_vs_time =\
-        simulate_collisions(xi, masses, dV, dt, t_end, store_every)
-        # simulate_collisions_np(xi, masses, dV, dt, t_end, store_every)
+        simulate_collisions_np(xis, masses, dV, dt, t_end, store_every,
+                               kernel=kernel)
+        # simulate_collisions(xi, masses, dV, dt, t_end, store_every)
     
     np.save(path + f"conc_{sim_n}.npy",concentrations)
     np.save(path + f"times_{sim_n}.npy",times)

collision_box_model_plotting.py
@@ -12,45 +12,66 @@ import matplotlib.pyplot as plt
 import constants as c
 from microphysics import compute_radius_from_mass
 
+from analysis import auto_bin_SIPs
 
-simdata_path = "/home/jdesk/OneDrive/python/sim_data/"
-# folder = "collision_box_model_multi_sim/"
-# folder = "collision_box_model_multi_sim/dV_4E-3_no_sim_50/"
-# folder = "collision_box_model_multi_sim/dV_1E0_no_spc_40_no_sim_2/"
-# folder = "collision_box_model_multi_sim/dV_1E0_no_spc_80_no_sim_50/"
-# folder = "collision_box_model_multi_sim/dV_1E0_no_spc_80_no_sim_50_02/"
-# folder = "collision_box_model_multi_sim/dV_1E-3_no_spc_40_no_sim_2/"
-# folder = "collision_box_model_multi_sim/dV_1E-3_no_spc_40_no_sim_5/"
-# folder = "collision_box_model_multi_sim/dV_1E-1_no_spc_40_no_sim_50/"
-# folder = "collision_box_model_multi_sim/dV_1E0_no_spc_40_no_sim_50/"
-# folder = "collision_box_model_multi_sim/dV_1E0_no_spc_40_no_sim_50_eps_500/"
-# folder = "collision_box_model_multi_sim/dV_1E0_no_spc_40_no_sim_50_eps_500_pmax_1E-7/"
-# folder = "collision_box_model_multi_sim/dV_1E0_no_spc_80_no_sim_50_eps_500_pmax_1E-7/"
-# folder = "collision_box_model_multi_sim/dV_1E0_no_spc_40_no_sim_50_eps_100_pmax_1E-7/"
-folder = "collision_box_model_multi_sim/dV_1E0_no_spc_80_no_sim_50_eps_200_pmax_1E-7/"
-# folder = "collision_box_model/"
-path = simdata_path + folder
 
+# simdata_path = "/home/jdesk/OneDrive/python/sim_data/"
+# folder = "collision_box_model_multi_sim/dV_1E0_kappa_40_no_sim_50/"
+# path = simdata_path + folder
 # path = "/mnt/D/sim_data/dV_4E-3_no_sim_50/"
 
-t_end = 3600.0
-dt = 1.0
-# dV = 1.0E-1
-# dV = 4.0E-3
 dV = 1.0
+# dV = 1.0E-6
+dt = 1.0
+# dt = 1.0
+store_every = 600
+t_end = 3600.0
+
+no_spc = 40
+
+kernel = "long"
+init = "SingleSIP"
+
 no_sims = 50
-# no_sims = 30
+seed_list = np.arange(4711, 4711+no_sims*2, 2)
+# seed_list = np.arange(4711+38, 4711+38+no_sims*2, 2)
+
+p_min = 0
+p_max = 0.9999999
+# bin linear spreading parameter
+eps = 200
+
+# for SingleSIP random
+kappa = 40
 
 # droplet concentration
 #n = 100 # cm^(-3)
 n0 = 297.0 # cm^(-3)
 # liquid water content (per volume)
 LWC0 = 1.0E-6 # g/cm^3
-# print(np.arange(0.0,t_end,dt))
-# print(len(np.arange(0.0,t_end,dt)))
-
 # total number of droplets
-no_spct = int(n0 * dV * 1.0E6)
+no_rpc = int(n0 * dV * 1.0E6)
+print("no_rpc=", no_rpc)
+
+# we start with a monomodal exponential distribution
+# mean droplet mass
+mu = 1.0E15*LWC0 / n0
+print("mu_m=", mu)
+mu_R = compute_radius_from_mass(mu, c.mass_density_water_liquid_NTP)
+print("mu_R=", mu_R)
+total_mass_in_cell = dV*LWC0*1.0E6*1.0E15 # in fg = 1.0E-18 kg
+
+simdata_path = "/home/jdesk/OneDrive/python/sim_data/"
+# folder = "collision_box_model_multi_sim/dV_1E0_no_spc_80_no_sim_50_eps_200_pmax_1E-7/"
+# folder = "collision_box_model_multi_sim/dV_1E0_kappa_40_no_sim_50/"
+if init == "SingleSIP":
+    folder =\
+f"collision_box_model/kernels/{kernel}/init_{init}_dV_{dV}_kappa_{kappa}_no_sims_{no_sims}/"
+elif init == "xi_random":
+    folder =\
+f"collision_box_model/kernels/{kernel}/init_{init}_dV_{dV}_no_spc_{no_spc}_eps_{eps}no_sims_{no_sims}/"
+path = simdata_path + folder
+
 
 times = []
 conc = []
@@ -102,302 +123,6 @@ ax.set_xlim([0.0,60.0])
 ax.set_ylim([1.0E6,1.0E9])
 ax.grid()
 
-#%% BINNING OF SIPs:
-
-def auto_bin_SIPs(masses, xis, xi_min, no_bins, dV, no_sims):
-
-    ind = np.nonzero(xis)
-    m_sort = masses[ind]
-    xi_sort = xis[ind]
-    # print(m_sort.shape)
-    # print(xi_sort.shape)
-    # print()
-    
-    ind = np.argsort(m_sort)
-    m_sort = m_sort[ind]
-    xi_sort = xi_sort[ind]
-    # print(m_sort.shape)
-    # print(xi_sort.shape)
-    
-    # plt.plot(masses, "o")
-    # plt.plot(m_sort, "o")
-    # print(np.nonzero(xis))
-    
-    # plt.loglog(m_sort, xi_sort, "+")
-    # plt.plot(m_sort, xi_sort, "+")
-    
-    ### merge particles with xi < xi_min
-    for i in range(len(xi_sort)-1):
-        if xi_sort[i] < xi_min:
-            xi = xi_sort[i]
-            m = m_sort[i]
-            xi_left = 0
-            j = i
-            while(j > 0 and xi_left==0):
-                j -= 1
-                xi_left = xi_sort[j]
-            if xi_left != 0:
-                m1 = m_sort[j]
-                dm_left = m-m1
-            else:
-                dm_left = 1.0E18
-            m2 = m_sort[i+1]
-            if m2-m < dm_left:
-                j = i+1
-                # assign to m1 since distance is smaller
-                # i.e. increase number of xi[i-1],
-                # then reweight mass to total mass
-            m_sum = m*xi + m_sort[j]*xi_sort[j]
-            # print("added pt ", i, "to", j)
-            # print("xi_i bef=", xi)
-            # print("xi_j bef=", xi_sort[j])
-            # print("m_i bef=", m)
-            # print("m_j bef=", m_sort[j])
-            # xi_sort[j] += xi_sort[i]
-            # m_sort[j] = m_sum / xi_sort[j]
-            # print("xi_j aft=", xi_sort[j])
-            # print("m_j aft=", m_sort[j])
-            xi_sort[i] = 0           
-    
-    if xi_sort[-1] < xi_min:
-        i = -1
-        xi = xi_sort[i]
-        m = m_sort[-1]
-        xi_left = 0
-        j = i
-        while(xi_left==0):
-            j -= 1
-            xi_left = xi_sort[j]
-        
-        m_sum = m*xi + m_sort[j]*xi_sort[j]
-        xi_sort[j] += xi_sort[i]
-        m_sort[j] = m_sum / xi_sort[j]
-        xi_sort[i] = 0           
-    
-    ind = np.nonzero(xi_sort)
-    xi_sort = xi_sort[ind]
-    m_sort = m_sort[ind]
-    
-    # print()
-    # print("xis.min()")
-    # print("xi_sort.min()")
-    # print(xis.min())
-    # print(xi_sort.min())
-    # print()
-    
-    # print("xi_sort.shape, xi_sort.dtype")
-    # print("m_sort.shape, m_sort.dtype")
-    # print(xi_sort.shape, xi_sort.dtype)
-    # print(m_sort.shape, m_sort.dtype)
-    
-    # plt.loglog(m_sort, xi_sort, "x")
-    # plt.plot(m_sort, xi_sort, "x")
-    
-    # resort, if masses have changed "places" in the selection process
-    ind = np.argsort(m_sort)
-    m_sort = m_sort[ind]
-    xi_sort = xi_sort[ind]
-    
-    ### merge particles, which have masses or xis < m_lim, xi_lim
-    no_bins0 = no_bins
-    no_bins *= 10
-    # print("no_bins")
-    # print("no_bins0")
-    # print(no_bins)
-    # print(no_bins0)
-    
-    no_spc = len(xi_sort)
-    n_save = int(no_spc//1000)
-    if n_save < 2: n_save = 2
-    
-    no_rpc = np.sum(xi_sort)
-    total_mass = np.sum(xi_sort*m_sort)
-    xi_lim = no_rpc / no_bins
-    m_lim = total_mass / no_bins
-    
-    bin_centers = []
-    m_bin = []
-    xi_bin = []
-    
-    n_left = no_rpc
-    
-    i = 0
-    while(n_left > 0 and i < len(xi_sort)-n_save):
-        bin_mass = 0.0
-        bin_xi = 0
-        # bin_center = 0.0
-        
-        # while(bin_xi < n_lim):
-        while(bin_mass < m_lim and bin_xi < xi_lim and n_left > 0
-              and i < len(xi_sort)-n_save):
-            bin_xi += xi_sort[i]
-            bin_mass += xi_sort[i] * m_sort[i]
-            n_left -= xi_sort[i]
-            i += 1
-        bin_centers.append(bin_mass / bin_xi)
-        m_bin.append(bin_mass)
-        xi_bin.append(bin_xi)
-            
-    # return m_bin, xi_bin, bin_centers    
-    
-    xi_bin = np.array(xi_bin)
-    bin_centers = np.array(bin_centers)
-    m_bin = np.array(m_bin)
-    
-    ### merge particles, whose masses are close together in log space:
-    bin_size_log =\
-        (np.log10(bin_centers[-1]) - np.log10(bin_centers[0])) / no_bins0
-    
-    # print("np.sum(xi_bin*bin_centers), m_bin.sum() before")
-    # print(np.sum(xi_bin*bin_centers), m_bin.sum())
-    
-    i = 0
-    while(i < len(xi_bin)-1):
-        m_next_bin = bin_centers[i] * 10**bin_size_log
-        m = bin_centers[i]
-        j = i
-        while (m < m_next_bin and j < len(xi_bin)-1):
-            j += 1
-            m = bin_centers[j]
-        if m >= m_next_bin:
-            j -= 1
-        if (i != j):
-            m_sum = 0.0
-            xi_sum = 0
-            for k in range(i,j+1):
-                m_sum += m_bin[k]
-                xi_sum += xi_bin[k]
-                if k > i:
-                    xi_bin[k] = 0
-            bin_centers[i] = m_sum / xi_sum
-            xi_bin[i] = xi_sum
-            m_bin[i] = m_sum
-        i = j+1            
-    
-    
-    ind = np.nonzero(xi_bin)
-    xi_bin = xi_bin[ind]
-    bin_centers = bin_centers[ind]        
-    m_bin = m_bin[ind]
-    
-    # print("np.sum(xi_bin*bin_centers), m_bin.sum() after")
-    # print(np.sum(xi_bin*bin_centers), m_bin.sum())
-    
-    ######
-    # bin_size = 0.5 * (bin_centers[-1] - bin_centers[0]) / no_bins0
-    # bin_size = (bin_centers[-1] - bin_centers[0]) / no_bins0
-    
-    # print("len(bin_centers) bef =", len(bin_centers))
-    
-    # for i, bc in enumerate(bin_centers[:-1]):
-    #     if bin_centers[i+1] - bc < bin_size and xi_bin[i] != 0:
-    #         m_sum = m_bin[i+1] + m_bin[i]
-    #         xi_sum = xi_bin[i+1] + xi_bin[i]
-    #         bin_centers[i] = m_sum / xi_sum
-    #         xi_bin[i] = xi_sum
-    #         xi_bin[i+1] = 0
-    #         m_bin[i] = m_sum
-
-    # ind = np.nonzero(xi_bin)
-    # xi_bin = xi_bin[ind]
-    # m_bin = m_bin[ind]
-    # bin_centers = bin_centers[ind]
-    ######
-    
-    # print("len(bin_centers) after =", len(bin_centers))
-
-    # radii = compute_radius_from_mass(m_sort, c.mass_density_water_liquid_NTP)
-    radii = compute_radius_from_mass(bin_centers,
-                                     c.mass_density_water_liquid_NTP)
-    
-    ###
-    # find the midpoints between the masses/radii
-    # midpoints = 0.5 * ( m_sort[:-1] + m_sort[1:] )
-    # m_left = 2.0 * m_sort[0] - midpoints[0]
-    # m_right = 2.0 * m_sort[-1] - midpoints[-1]
-    bins = 0.5 * ( radii[:-1] + radii[1:] )
-    # add missing bin borders for m_min and m_max:
-    R_left = 2.0 * radii[0] - bins[0]
-    R_right = 2.0 * radii[-1] - bins[-1]
-    
-    bins = np.hstack([R_left, bins, R_right])
-    bins_log = np.log(bins)
-    # print(midpoints)
-       
-    # mass_per_ln_R = m_sort * xi_sort
-    # mass_per_ln_R *= 1.0E-15/no_sims
-    
-    m_bin = np.array(m_bin)
-    
-    g_ln_R = m_bin * 1.0E-15 / no_sims / (bins_log[1:] - bins_log[0:-1]) / dV
-    
-    return g_ln_R, radii, bins, xi_bin, bin_centers
-
-# masses = masses_vs_time[3]
-# xis = xis_vs_time[3]
-# radii = compute_radius_from_mass(masses, c.mass_density_water_liquid_NTP)
-# xi_min = 100
-# m_bin, xi_bin, bins = auto_bin_SIPs(masses, xis, xi_min)
-
-# r_bin = compute_radius_from_mass(bins, c.mass_density_water_liquid_NTP)
-
-# plt.plot(r_bin, m_bin, "o")
-
-# masses = masses_vs_time[3]
-# xis = xis_vs_time[3]
-# radii = compute_radius_from_mass(masses, c.mass_density_water_liquid_NTP)
-# print(masses.shape)
-# print(xis.shape)
-# print()
-
-# xi_min = 100
-# no_bins = 40
-# g_ln_R, R_sort, bins, xi_bi, m_bins = auto_bin_SIPs(masses,
-#                                                     xis, xi_min, no_bins,
-#                                                     dV, no_sims)
-
-# fig = plt.figure()
-# ax = plt.gca()
-# ax.loglog(R_sort, g_ln_R, "x")
-# # ax.plot(R_sort, g_ln_R, "x")
-# # ax.plot(R_sort, xi_bin)
-
-# ###
-
-# method = "log_R"
-
-# R_min = 0.99*np.amin(radii)
-# R_max = 1.01*np.amax(radii)
-# # R_max = 3.0*np.amax(radii)
-# print("R_min=", R_min)
-# print("R_max=", R_max)
-
-# no_bins = 20
-# if method == "log_R":
-#     bins = np.logspace(np.log10(R_min), np.log10(R_max), no_bins)
-# elif method == "lin_R":
-#     bins = np.linspace(R_min, R_max, no_bins)
-# # print(bins)
-
-# # masses in 10^-15 gram
-# mass_per_ln_R, _ = np.histogram(radii, bins, weights=masses*xis)
-# # convert to gram
-# mass_per_ln_R *= 1.0E-15/no_sims
-# # print(mass_per_ln_R)
-# # print(mass_per_ln_R.shape, bins.shape)
-
-# bins_log = np.log(bins)
-# # bins_mid = np.exp((bins_log[1:] + bins_log[:-1]) * 0.5)
-# bins_mid = (bins[1:] + bins[:-1]) * 0.5
-
-# g_ln_R = mass_per_ln_R / (bins_log[1:] - bins_log[0:-1]) / dV
-
-# # print(g_ln_R.shape)
-# # print(np.log(bins_mid[1:])-np.log(bins_mid[0:-1]))
-# ax.loglog( bins_mid, g_ln_R, "-" )
-###
-
-
 #%% PLOT MASS DENSITY DISTRIBUTIONS VS RADIUS
 
 # we need g_ln_R vs R, where g_ln_R = 3 * m^2 * f_m(m)
@@ -407,14 +132,14 @@ def auto_bin_SIPs(masses, xis, xi_min, no_bins, dV, no_sims):
 
 # dV = 0.1**3
 
-no_bins = 30
+no_bins = 25
 xi_min = 1
 
 fig_name = path + f"mass_distr_per_ln_R_vs_time_no_sims_{no_sims}_dV_{dV}.png"
 fig, ax = plt.subplots(figsize=(8,8))
 
 # method = "lin_R"
-# method = "log_R"
+method = "log_R"
 
 # for ind_t in range(1):
 for ind_t in range(len(times)):
@@ -435,42 +160,48 @@ for ind_t in range(len(times)):
     print("m_ges =", np.sum(masses*xis))
     print("no_rpc =", np.sum(xis))
     
+    R_min *=0.99
+    R_max *= 1.01
+    
     # no_bins = 20
-    # if method == "log_R":
-    #     bins = np.logspace(np.log10(R_min), np.log10(R_max), no_bins)
-    # elif method == "lin_R":
-    #     bins = np.linspace(R_min, R_max, no_bins)
-    # # print(bins)
+    if method == "log_R":
+        bins = np.logspace(np.log10(R_min), np.log10(R_max), no_bins)
+    elif method == "lin_R":
+        bins = np.linspace(R_min, R_max, no_bins)
+    # print(bins)
     
-    # # masses in 10^-15 gram
-    # mass_per_ln_R, _ = np.histogram(radii, bins, weights=masses*xi)
-    # # convert to gram
-    # mass_per_ln_R *= 1.0E-15/no_sims
-    # # print(mass_per_ln_R)
-    # # print(mass_per_ln_R.shape, bins.shape)
+    # masses in 10^-15 gram
+    mass_per_ln_R, _ = np.histogram(radii, bins, weights=masses*xis)
+    # convert to gram
+    mass_per_ln_R *= 1.0E-15/no_sims
+    # print(mass_per_ln_R)
+    # print(mass_per_ln_R.shape, bins.shape)
     
-    # bins_log = np.log(bins)
-    # # bins_mid = np.exp((bins_log[1:] + bins_log[:-1]) * 0.5)
-    # bins_mid = (bins[1:] + bins[:-1]) * 0.5
+    bins_log = np.log(bins)
+    # bins_mid = np.exp((bins_log[1:] + bins_log[:-1]) * 0.5)
+    bins_mid = (bins[1:] + bins[:-1]) * 0.5
     
-    # g_ln_R = mass_per_ln_R / (bins_log[1:] - bins_log[0:-1]) / dV
+    g_ln_R = mass_per_ln_R / (bins_log[1:] - bins_log[0:-1]) / dV
     
     # print(g_ln_R.shape)
     # print(np.log(bins_mid[1:])-np.log(bins_mid[0:-1]))
 
-    
-    g_ln_R, bins_mid, bins, xi_bin, mass_bin =\
-        auto_bin_SIPs(masses, xis, xi_min, no_bins, dV, no_sims)
-    
-    print("m_ges after binning=", np.sum(xi_bin*mass_bin))
-    print("no_rpc =", np.sum(xi_bin))        
+    ###
+    # g_ln_R, bins_mid, bins, xi_bin, mass_bin =\
+    #     auto_bin_SIPs(masses, xis, xi_min, no_bins, dV, no_sims)
+    # print("m_ges after binning=", np.sum(xi_bin*mass_bin))
+    # print("no_rpc =", np.sum(xi_bin))        
+    ###
     
     ax.loglog( bins_mid, g_ln_R, "-" )
     # ax.loglog( bins_mid, g_ln_R, "o", markersize=5.0 )
     
 
 # ax.loglog( bins_mid, np.ones_like(bins_mid), "o" )
-ax.set_xlim([1.0,1.0E3])
+if kernel == "golovin":
+    ax.set_xlim([1.0,1.0E3])
+elif kernel == "long":
+    ax.set_xlim([1.0,8.0E3])
 # ax.hist(radii,weights=masses*xi,bins=30)
 ax.set_ylim([1.0E-4,1.0E1])
 # ax.set_ylim([1.0E-6,1.0E1])
@@ -480,12 +211,12 @@ ax.set_xlabel("radius (mu)")
 ax.set_ylabel("mass distribution per ln(R) and volume (g/m^3)")
 ax.set_title(
 f"#sims={no_sims}, n0={n0:.3} cm^-3, LWC0={LWC0} g/cm^3, dV={dV} \
-#SIP={no_spct:.2e}", pad = 10)
+#SIP={no_spc:.2e}", pad = 10)
 ax.grid()
 
 fig.savefig(fig_name)
 
-#%%
+#%% PLOT SIP TOTAL MASSES
 fig, ax = plt.subplots(figsize=(8,8))
 for ind_t in range(len(times)):
     masses = masses_vs_time[ind_t]

init.py
@@ -451,28 +451,31 @@ def generate_random_positions(grid, no_spc, seed):
     rel_pos = np.array([rnd_x, rnd_z])
     return pos, rel_pos
 
-#%% GENERATE SIP ENSEMBLE
+#%% GENERATE SIP ENSEMBLES
 
 # par = "rate" parameter "k" of the expo distr: k*exp(-k*m) (in 10^18 kg)
 # no_rpc = number of real particles in cell
 def generate_SIP_ensemble_expo_SingleSIP_weak_threshold(
-        par, no_rpc, r_critmin=0.6, m_high_by_m_low=1.0E6, kappa=40, eta=1.0E-9,
-        seed=4711, setseed = True):
+        par, no_rpc, r_critmin=0.6, m_high_by_m_low=1.0E6, kappa=40,
+        eta=1.0E-9, seed=4711, setseed = True):
     
     if setseed: np.random.seed(seed)
     m_low = compute_mass_from_radius(r_critmin, c.mass_density_NaCl_dry)
     # m_high = num_int_expo_impl_right_border(0.0, p_max, 1.0/par*0E-6, par,
     #                                            cnt_lim=1E8)
     m_high = m_low * m_high_by_m_low
-    # since we consider only particles with m > m_low, the distribution has
-    # to be adjusted. We have two possibilities:
+    # since we consider only particles with m > m_low, the total number of
+    # placed particles and the total placed mass will be underestimated
+    # to fix this, we could adjust the PDF
+    # by e.g. one of the two possibilities:
     # 1. decrease the total number of particles and thereby the total pt conc.
     # 2. keep the total number of particles, and thus increase the PDF
     # in the interval [m_low, m_high]
-    # I choose 1.: decrease the total number of particles by multiplication 
-    # with factor num_int_expo_np(m_low, m_high, par, steps=1.0E6)
-    print(no_rpc)
-    no_rpc *= num_int_expo(m_low, m_high, par, steps=1.0E6)
+    # # For 1.: decrease the total number of particles by multiplication 
+    # # with factor num_int_expo_np(m_low, m_high, par, steps=1.0E6)
+    # print(no_rpc)
+    # no_rpc *= num_int_expo(m_low, m_high, par, steps=1.0E6)
+    
     # for 2.:
     # increase the total number of
     # real particle "no_rpc" by 1.0 / int_(m_low)^(m_high)
@@ -490,14 +493,15 @@ def generate_SIP_ensemble_expo_SingleSIP_weak_threshold(
     m_left = m_low
     m = 0.0
     
-    print("no_rpc =", no_rpc)
-    while(no_rp_set < no_rpc and m < m_high):
+    # print("no_rpc =", no_rpc)
+    # while(no_rp_set < no_rpc and m < m_high):
+    while(m < m_high):
         m_right = m_left * 10**kappa_inv
-        print("missing particles =", no_rpc - no_rp_set)
-        print("m_left, m_right")
-        print(bin_n, m_left, m_right)
+        # print("missing particles =", no_rpc - no_rp_set)
+        # print("m_left, m_right, m_high")
+        # print(bin_n, m_left, m_right, m_high)
         m = np.random.uniform(m_left, m_right)
-        print("m =", m)
+        # print("m =", m)
         # we do not round to integer here, because of the weak threshold below
         # the rounding is done afterwards
         xi = dst_expo(m,par) * (m_right - m_left) * no_rpc
@@ -505,7 +509,8 @@ def generate_SIP_ensemble_expo_SingleSIP_weak_threshold(
         # if xi < 1 : xi = 1
         if no_rp_set + xi > no_rpc:
             xi = no_rpc - no_rp_set
-        print("xi =", xi)
+            # print("no_rpc reached")
+        # print("xi =", xi)
         masses.append(m)
         xis.append(xi)
         no_rp_set += xi
@@ -517,8 +522,11 @@ def generate_SIP_ensemble_expo_SingleSIP_weak_threshold(
     masses = np.array(masses)
     
     xi_max = xis.max()
-    print("xi_max =", xi_max)
+    # print("xi_max =", f"{xi_max:.2e}")
+    
     xi_critmin = int(xi_max * eta)
+    if xi_critmin < 1: xi_critmin = 1
+    # print("xi_critmin =", xi_critmin)
     
     for bin_n, xi in enumerate(xis):
         if xi < xi_critmin:
@@ -540,54 +548,8 @@ def generate_SIP_ensemble_expo_SingleSIP_weak_threshold(
     # and assign the average mass to it
     # then reweight all of the masses such that the total mass is right.
     
-    return xis, masses
-
-dV = 1.0
-# dV = 1.0E-6
-dt = 1.0
-# dt = 1.0
-store_every = 600
-t_end = 3600.0
-
-no_spc = 80
-
-# droplet concentration
-#n = 100 # cm^(-3)
-n0 = 297.0 # cm^(-3)
-# liquid water content (per volume)
-LWC0 = 1.0E-6 # g/cm^3
-# total number of droplets
-no_rpc = int(n0 * dV * 1.0E6)
-print("no_rpc=", no_rpc)
-
-# we start with a monomodal exponential distribution
-# mean droplet mass
-mu = 1.0E15*LWC0 / n0
-print("mu_m=", mu)
-mu_R = compute_radius_from_mass(mu, c.mass_density_water_liquid_NTP)
-print("mu_R=", mu_R)
-total_mass_in_cell = dV*LWC0*1.0E6*1.0E15 # in fg = 1.0E-18 kg
-
-r_critmin = 0.6
-kappa = 40
-eta = 1.0E-9
-seed = 4711
-   
-xis, masses = generate_SIP_ensemble_expo_SingleSIP_weak_threshold(
-                  1.0/mu, no_rpc)
-
-print(xis.shape)
-print(masses.shape)
-print((xis.sum()-no_rpc)/no_rpc)
-print((np.sum(masses*xis)-total_mass_in_cell)/total_mass_in_cell)
-
-import matplotlib.pyplot as plt
-Rs = compute_radius_from_mass(masses, c.mass_density_water_liquid_NTP)
-plt.plot()
+    return masses, xis, m_low, m_high
     
-#%%  
-
-
 # no_spc is the intended number of super particles per cell,
 # this will right on average, but will vary due to the random assigning 
 # process of the xi_i